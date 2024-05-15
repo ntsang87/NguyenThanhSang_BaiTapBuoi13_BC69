09115901a3016104a5def5fdfd903a01f2eef40f
ntsang87 <thanhsangvt206@gmail.com>
2024-05-15 16:17:06 +0700

    Xong bai 2

---
+++ index.js
@@ -0,0 +1,17 @@
+// // Bài tập 1
+// Đầu vào: lương 1 ngày cố định là 100.000VND & nhập số ngày làm
+// Đầu ra: tính lương dựa vào lương 1 ngày & số ngày làm
+// Công thức: luong = số ngày làm * 100.000VND
+document.getElementById("tinhLuong").onclick = function () {
+  const luong1Ngay = new Intl.NumberFormat("vn-Vi").format(100000);
+  let soNgayLam = document.getElementById("soNgayLam").value * 1;
+  let tienLuong = luong1Ngay * soNgayLam;
+  console.log(tienLuong);
+  let thePBai1 = document.querySelector(".kqBai1");
+  thePBai1.innerHTML = "Tiền lương là: " + luong1Ngay;
+};
+
+// Bài tập 2
+// Đầu vào: Nhập vào giá trị 5 con số
+// Đầu ra: Tính số trung bình của 5 số trên
+// Công thức: Tổng giá trị 5 số / 5
